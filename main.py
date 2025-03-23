c5fa4e179744171ee8197b3df7325ff47db3023f
Tomshus <kazuonat2562004@gmail.com>
2025-03-23 19:52:15 +0700

    rename module -> modules

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from constants import PROJECT_ROOT, MODULE_DIR
 # Add the project root and module directories to Python path
 sys.path.extend([PROJECT_ROOT, MODULE_DIR])
 
-from module.extraction import extract_features
-from module.api_integration import (
+from modules.extraction import extract_features
+from modules.api_integration import (
     check_openphish,
     check_google_safe_browsing,
     check_virustotal,
